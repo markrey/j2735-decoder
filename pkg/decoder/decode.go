998d37d3527ec89b2c0370190ba36693478d2b15
hsuyu <yu.hsiang.sean.hsu@one.verizon.com>
2019-07-21 19:20:07 -0400

    convert to using builtin xml print

--- pkg/decoder/decode.go
+++ pkg/decoder/decode.go
@@ -3,36 +3,78 @@ package decoder
 // #cgo CFLAGS: -I ./c/
 // #cgo LDFLAGS: -L ./c/ -lasncodec
 // #include <MessageFrame.h>
+// #include <xer_encoder.h>
 // void free_struct(asn_TYPE_descriptor_t descriptor, void* frame) {
 // 		ASN_STRUCT_FREE(descriptor, frame);
+// }
+// int xer__print2s (const void *buffer, size_t size, void *app_key)
+// {
+//     char *string = (char *) app_key;
+//     strncat(string, buffer, size);
+//     return 0;
+// }
+// int xer_sprint(void *string, asn_TYPE_descriptor_t *td, void *sptr)
+// {
+//     asn_enc_rval_t er;
+//     er = xer_encode(td, sptr, XER_F_CANONICAL, xer__print2s, string);
+//     if (er.encoded == -1)
+//         return -1;
+//     return er.encoded;
 // }
 import "C"
 import (
 	"fmt"
+	"strings"
 	"unsafe"
+
+	xj "github.com/basgys/goxml2json"
 )
 
-// Message ID values as specified in J2735 specifications
+// FormatType is used to identify which format to decode
+type FormatType int
+
+// Type of decoding supported by module
 const (
-	PSMID  int64 = 32
-	RSAID  int64 = 27
-	BSMID  int64 = 20
-	SPaTID int64 = 19
-	MAPID  int64 = 18
+	XML  FormatType = iota
+	JSON FormatType = iota
 )
 
 // Decode is a public function for other packages to decode
-func Decode(bytes []byte, length uint64) *MessageFrame {
+func Decode(bytes []byte, length uint64, format FormatType) string {
 	msgFrame := decodeMessageFrame(&C.asn_DEF_MessageFrame, bytes, length)
 	defer C.free_struct(C.asn_DEF_MessageFrame, unsafe.Pointer(msgFrame))
-	var message MessageValue
-	Logger.Infof("Message %d decoded succesfully", int64(msgFrame.messageId))
-	switch int64(msgFrame.messageId) {
-	case BSMID:
-		message = &BasicSafetyMessage{}
+	Logger.Infof("Decoding message type: %d", int64(msgFrame.messageId))
+
+	size := 2048
+	var buffer []byte
+	for true {
+		buffer = make([]byte, size)
+		bufPtr := unsafe.Pointer(&buffer[0])
+		rval := C.xer_sprint(bufPtr, &C.asn_DEF_MessageFrame, unsafe.Pointer(msgFrame))
+		Logger.Infof("Bytes Encoded: %d", int(rval))
+		if int(rval) == -1 {
+			err := "Cannot encode message!"
+			Logger.Error(err)
+			panic(err)
+		} else if int(rval) > len(buffer) {
+			size = int(rval)
+		}
 		break
 	}
-	return message.Create(msgFrame)
+	xmlStr := fmt.Sprintf("%s", buffer)
+	switch format {
+	case XML:
+		return xmlStr
+	case JSON:
+		xml := strings.NewReader(xmlStr)
+		json, err := xj.Convert(xml)
+		if err != nil {
+			Logger.Errorf("Cannot encode to JSON: %s", err)
+			panic(err)
+		}
+		return json.String()
+	}
+	return ""
 }
 
 // decodeMessageFrame requires caller to free the MessageFrame returned
--- pkg/decoder/decoder_test.go
+++ pkg/decoder/decoder_test.go
@@ -1,7 +1,47 @@
 package decoder_test
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
 
-func TestDecode(t *testing.T) {
+	"github.com/yh742/j2735-decoder/pkg/decoder"
+	"gotest.tools/assert"
+)
 
+func TestBSMXML(t *testing.T) {
+	file, err := os.Open("./test/bsm1.uper")
+	defer file.Close()
+	assert.NilError(t, err)
+	xml, err := os.Open("./test/bsm1.xml")
+	defer xml.Close()
+	bytes := make([]byte, 1024)
+	read, err := file.Read(bytes)
+	assert.NilError(t, err)
+	decodedMsg := decoder.Decode(bytes, uint64(read), decoder.XML)
+	read, err = xml.Read(bytes)
+	assert.NilError(t, err)
+	xmlStr := fmt.Sprintf("%s", bytes)
+	assert.Equal(t, decodedMsg[:802], xmlStr[:802])
+}
+
+func TestBSMJSON(t *testing.T) {
+	file, err := os.Open("./test/bsm2.uper")
+	defer file.Close()
+	assert.NilError(t, err)
+	bytes := make([]byte, 2048)
+	read, err := file.Read(bytes)
+	assert.NilError(t, err)
+	decodedMsg := decoder.Decode(bytes, uint64(read), decoder.JSON)
+	assert.NilError(t, err)
+	var jsonMap map[string]interface{}
+	var ok bool
+	json.Unmarshal([]byte(decodedMsg), &jsonMap)
+	jsonMap, ok = jsonMap["MessageFrame"].(map[string]interface{})
+	assert.Assert(t, ok)
+	t.Log(jsonMap)
+	id, isString := jsonMap["messageId"].(string)
+	assert.Assert(t, isString)
+	assert.Equal(t, id, "20")
 }
